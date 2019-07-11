3759adb870228246d0cb4d5eb63bdabb818dcbac
Chithambaram <chithambaram.n@cliffex.com>
2019-07-11 14:20:04 +0530

    7FUnctionComponentsPractices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function MyInfo(){
+    return(
+        <div>
+            <h1>Chithambaram</h1>
+            <p>Hi hello How are u</p>
+            <ul>
+                <li>place1</li>
+                <li>place2</li>
+                <li>place3</li>
+            </ul>
+        </div>
+    )
+}
+ReactDOM.render(<MyInfo/>,document.getElementById("root"));
