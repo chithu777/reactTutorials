e18b0fdce1cec7de290c958e4dd4584e2477b0fe
Chithambaram <chithambaram.n@cliffex.com>
2019-07-11 15:30:31 +0530

    Arrow Function Sample

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-function App() {
-  return (
-    <body>
-      <Header />
-      <Main />
-      <Footer /> 
-    </body>
-
-  )
-}
+const App =() =>  <body> <Header /> <Main /> <Footer /> </body>
 
 export default App;
