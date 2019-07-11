3469075a2ef90df9ea185269381a2aca64ff08d5
Chithambaram <chithambaram.n@cliffex.com>
2019-07-11 14:30:55 +0530

    8ComponentINSeperateFiles

---
+++ src/component/MyInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function MyInfo(){
+    return(
+        <div>
+            <h1>Chithambaram</h1>
+            <p>Hi hello How are u</p>
+            <ul>
+                <li>place1</li>
+                <li>place2</li>
+                <li>place3</li>
+            </ul>
+        </div>
+    )
+}
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-function MyInfo(){
-    return(
-        <div>
-            <h1>Chithambaram</h1>
-            <p>Hi hello How are u</p>
-            <ul>
-                <li>place1</li>
-                <li>place2</li>
-                <li>place3</li>
-            </ul>
-        </div>
-    )
-}
+import MyInfo from './component/MyInfo';
+
 ReactDOM.render(<MyInfo/>,document.getElementById("root"));
