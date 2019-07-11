11d08a07ae710c391ee8eaf47cc892f87f074316
Chithambaram <chithambaram.n@cliffex.com>
2019-07-11 16:34:48 +0530

    Dealing with inline and styles object

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-const App =() =>  <body> <Header /> <Main /> <Footer /> </body>
+const App =() =>  {
+  return(
+    <div> <Header /> <Main /> <Footer /> </div>
+  )
+}
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,28 @@
 import React from 'react';
 function Main(){
+    const date =new Date(2018, 4, 4, 23);
+    const hours = date.getHours();
+    let myday;
+    const styles = {color:'red',fontSize:14};
+    if(hours < 12){
+        myday = "morning"
+        styles.color = "red"
+    }else if(hours >12 && hours < 17)
+    {
+        myday = "afternoon"
+        styles.color = "green"
+    }else if(hours >17 && hours < 19)
+    {
+        myday = "evening"
+        styles.color = "yellow"
+    }else{
+        myday = "night"
+        styles.color = "blue"
+    }
     return (
         <main>
-            <p>My Main Content</p>
+            <p style={styles}>Now!! {myday}</p>
         </main>
     )
 }
-
-
-
 export default Main
