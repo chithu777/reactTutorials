d432259d505aad9223581c1a4ad00ea1699a3799
Chithambaram <chithambaram.n@cliffex.com>
2019-07-18 17:17:13 +0530

    Checking CheckBox Value

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ class App extends Component{
 
   constructor(){
     super();
-    const ProductsData = productsData.map((data)=>{
-      return <TodoItem answer={data.answer} />
+    const ProductsData = productsData.map((data, index)=>{
+      return <TodoItem key={index} answer={data.answer} checkes={false} />
     });
     this.state = {"ProductsData":ProductsData}
+  }
+  changeContent(){
+    this.setState()
   }
   render(){
 
     return (
       <div>
+        <button onClick={this.changeContent()}>Click Me</button>
         {this.state.ProductsData}
       </div>
     )
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-function TodoItem(props){
-    const style = { color: "red",background:'pink'}
-    const style2 = { color: "red",background:'pink'}
-    const style3 = { color: "red"}
-    return(
-        <div style={style3}>
-            <p style={style}>{props.answer}</p>
-        </div>
-    )
+
+class TodoItem extends React.Component{
+    constructor(){
+        super();
+        const style = { color: "red",background:'pink'}
+        const style2 = { color: "red",background:'pink'}
+        const style3 = { color: "red"}
+    }
+    render(){
+        return(
+            <div style={this.style3}>
+    
+                <input type="checkbox" defaultChecked={this.props.checkes}></input>
+                <p style={this.style}>{this.props.answer}</p>
+            </div>
+        )
+    }
 }
 export default TodoItem
