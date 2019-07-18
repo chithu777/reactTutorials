220c0868045ecb8ec3008450bb214fb2089b5f40
Chithambaram <chithambaram.n@cliffex.com>
2019-07-18 12:44:58 +0530

    React States example 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React,{Component} from 'react';
 
-class App extends React.Component{
-    method(){
-      //this.setState({inputvalue:"New Chithu"});
-    }
-    constructor(){
-      super()
-      this.state= {name:"Chithu",age:"12"}
-    }
-    render(){
-      return(
-        <div>
-          <Header username="Chithu" />
-            <h1>Name is {this.state.name} and Age is {this.state.age}</h1> <button onClick = {this.method()} >ChangeValue</button>
-          <Footer/>
-        </div>
-      )
-    }
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {boolVals:false}
+  }
+  render(){
+
+    return (
+      <div>
+        you are currently logged {this.state.boolVals ? ('in'): ("Out")}
+      </div>
+    )
+  }
 }
 
 export default App
